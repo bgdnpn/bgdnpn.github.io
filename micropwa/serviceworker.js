0e763c6fcc210e2cfd09072adb2e9928ae3d2180
bgdnpn <bgdnpn@gmail.com>
2024-11-07 22:51:32 +0200

    move mPWA to separate repo

--- micropwa/serviceworker.js
+++
@@ -1,9 +0,0 @@
-var staticCacheName = "pwa";
- 
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
-    })
-  );
-});
